9f91a7c1f232b7df1243127b3222362034686e29
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-05-02 18:21:49 +0530

    session provider code changed

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,13 +1,17 @@
-"use client"
-import React from 'react'
-import { SessionProvider } from "next-auth/react"
+// Provider.tsx
+import { ReactNode } from 'react';
+import { SessionProvider } from "next-auth/react";
 
-function Provider({children}) {
-  return (
-    <SessionProvider >
-        {children}
-    </SessionProvider>
-  )
+interface ProviderProps {
+  children: ReactNode;
 }
 
-export default Provider
+function Provider({ children }: ProviderProps) {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  );
+}
+
+export default Provider;
