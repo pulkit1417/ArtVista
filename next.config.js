7aee8e2f8e01a09d511e22651825e33822671922
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-05-02 17:56:48 +0530

    webpack removed

--- next.config.js
+++ next.config.js
@@ -4,29 +4,3 @@ const nextConfig = {
         domains:['lh3.googleusercontent.com','firebasestorage.googleapis.com']
     }
 }
-
-
-const webpack = require('webpack');
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
-
-module.exports = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-      process: require.resolve('process/browser'), // Add this line
-    };
-
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        process: 'process/browser',
-        Buffer: ['buffer', 'Buffer'],
-      }),
-      new NodePolyfillPlugin()
-    );
-
-    return config;
-  },
-};
