3aff90c6e2922945ff91e7bfc8ad6c707a5a74e2
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-05-02 17:43:41 +0530

    metamask providers and webpack configured

--- next.config.js
+++ next.config.js
@@ -5,4 +5,24 @@ const nextConfig = {
     }
 }
 
-module.exports = nextConfig
+export default nextConfig
+// next.config.js
+import { ProvidePlugin } from 'webpack';
+
+export function webpack(config) {
+    config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+    };
+
+    config.plugins.push(
+        new ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer'],
+        })
+    );
+
+    return config;
+}
