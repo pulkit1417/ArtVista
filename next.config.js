eb957708af32d72c2d72f049470d2e3095a873e5
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-05-05 21:34:47 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -5,15 +5,3 @@ const nextConfig = {
     }
 }
 
-export default nextConfig
-
-import nextTranspileModules from 'next-transpile-modules';
-
-export function webpack(config, { isServer }) {
-    if (!isServer) {
-        config.resolve.fallback.buffer = require.resolve('buffer/');
-        config.resolve.fallback.stream = require.resolve('stream-browserify');
-    }
-
-    return nextTranspileModules(['node-fetch', 'encoding'])(config);
-}
